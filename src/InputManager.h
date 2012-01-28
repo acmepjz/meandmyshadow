c612c498b6cd2d0283bfc79c54e123a24bd2c4f2
Jz Pan <acmepjz@users.noreply.github.com>
2012-01-28 07:19:08 +0000

    Added joystick support to input manager.

--- src/InputManager.h
+++ src/InputManager.h
@@ -18,6 +18,8 @@
 #ifndef INPUTMANAGER_H
 #define INPUTMANAGER_H
 
+#include <SDL/SDL.h>
+#include <vector>
 #include <string>
 
 enum InputManagerKeys{
@@ -40,8 +42,8 @@ enum InputManagerKeys{
 
 class InputManager{
 public:
-	//constructor.
 	InputManager();
+	~InputManager();
 
 	//get and set key code of each key.
 	int getKeyCode(InputManagerKeys key);
@@ -66,6 +68,11 @@ public:
 	bool isKeyUpEvent(InputManagerKeys key);
 	//check if specified key is down.
 	bool isKeyDown(InputManagerKeys key);
+
+	//open all joysticks.
+	void openAllJoysitcks();
+	//close all joysticks.
+	void closeAllJoysticks();
 private:
 	//the key code of each key.
 	// - note of key code:
@@ -73,13 +80,23 @@ private:
 	//   1 to 4095 (0xFFF) means keyboard keys, 
 	//     currently SDLKey is less than 4095
 	//   >= 4096: bit field value means joystick.
-	//     TBA
+	//     0xWWXXYYZZ
+	//     WW = joystick index. currently unused, should be 0.
+	//     XX = joystick button type: 1-axis 2-button 3-hat, currently ball is unsupported
+	//     YY = joystick button index. (we assume joystick has at most 256 buttons)
+	//     ZZ = value. if type=axis then value should be 1 or 0xFF.
+	//                 if type=button then it's unused.
+	//                 if type=hat then it's SDL_HAT_LEFT, SDL_HAT_RIGHT, SDL_HAT_UP or SDL_HAT_DOWN.
 	int keys[INPUTMGR_MAX];
 	//the bit-field flag array saves the key states.
 	// 0x1 means the key is down.
 	// 0x2 means KeyDown event.
 	// 0x4 means KeyUp event.
 	int keyFlags[INPUTMGR_MAX];
+	//contains all joysticks.
+	std::vector<SDL_Joystick*> joysticks;
+	//internal function
+	int getKeyState(int keyCode,int oldState,bool hasEvent);
 };
 
 extern InputManager inputMgr;
