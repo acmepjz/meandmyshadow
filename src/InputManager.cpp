21527000bf8f8c3ef4e5fe76d0b3a74a22a4b34b
Jz Pan <acmepjz@users.noreply.github.com>
2012-02-01 15:44:59 +0000

    Now up/jump and down/action can be different key

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -30,14 +30,14 @@ InputManager inputMgr;
 
 //the order must be the same as InputManagerKeys
 static const char* keySettingNames[INPUTMGR_MAX]={
-	"key_up","key_down","key_left","key_right","key_space",
+	"key_up","key_down","key_left","key_right","key_jump","key_action","key_space",
 	"key_escape","key_restart","key_tab","key_save","key_load","key_swap",
 	"key_teleport","key_suicide","key_shift"
 };
 
 //the order must be the same as InputManagerKeys
 static const char* keySettingDescription[INPUTMGR_MAX]={
-	"Up (Jump)","Down (Action)","Left","Right","Space (Record)",
+	"Up (in menu)","Down (in menu)","Left","Right","Jump","Action","Space (Record)",
 	"Escape","Restart","Tab (View shadow/Level prop.)","Save game (in editor)","Load game","Swap (in editor)",
 	"Teleport (in editor)","Suicide (in editor)","Shift (in editor)"
 };
