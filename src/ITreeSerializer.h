19be98e194f3602995f149117b311b35d9a896a3
edwardlii <edwardlii@7fc951ab-27c7-4b2a-b7c6-f1d780be2054>
2012-07-15 12:26:51 +0000

    Fixed the copyright notice at the top of each source file.

--- src/ITreeSerializer.h
+++ src/ITreeSerializer.h
@@ -1,21 +1,21 @@
-/****************************************************************************
-** Copyright (C) 2011 Luka Horvat <redreaper132 at gmail.com>
-** Copyright (C) 2011 Edward Lii <edward_iii at myway.com>
-** Copyright (C) 2011 O. Bahri Gordebak <gordebak at gmail.com>
-**
-**
-** This file may be used under the terms of the GNU General Public
-** License version 3.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of
-** this file.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**
-**  You should have received a copy of the GNU General Public License
-**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-**
-****************************************************************************/
+/*
+ * Copyright (C) 2011-2012 Me and My Shadow
+ *
+ * This file is part of Me and My Shadow.
+ *
+ * Me and My Shadow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Me and My Shadow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Me and My Shadow.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef ITREESERIALIZER_H
 #define ITREESERIALIZER_H
