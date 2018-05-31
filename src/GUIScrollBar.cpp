d0ae644a3e8e7754c9646bb9d41507012e32394c
acme_pjz <acme_pjz@hotmail.com>
2018-05-31 23:34:22 +0800

    fix scroll bar highlight visual bug

--- src/GUIScrollBar.cpp
+++ src/GUIScrollBar.cpp
@@ -355,34 +355,37 @@ void GUIScrollBar::render(SDL_Renderer &renderer, int x, int y, bool draw){
 	calcPos();
 	
 	//Now the actual drawing begins.
-	if(orientation&&draw){
-		//The scrollbar is vertically orientated.
-		if(valuePerPixel>0){
-			//There are four buttons so draw them.
-            renderScrollBarButton(renderer, 2,x+left,y+top,x+left+width,y+top+height,-1,-1);
-            renderScrollBarButton(renderer,1,x+left,y+top,x+left+width,y+top+16,80,0);
-            renderScrollBarButton(renderer,3,x+left,y-1+(int)thumbStart,x+left+width,y+1+(int)thumbEnd,0,16);
-            renderScrollBarButton(renderer,5,x+left,y+top+height-16,x+left+width,y+top+height,96,0);
-		}else{
-			//There are two buttons so draw them.
-			int f=top+height/2;
-            renderScrollBarButton(renderer,1,x+left,y+top,x+left+width,y+1+f,80,0);
-            renderScrollBarButton(renderer,5,x+left,y+f,x+left+width,y+top+height,96,0);
-		}
-	}else{
-		//The scrollbar is horizontally orientated.
-		if(valuePerPixel>0){
-			//There are five buttons so draw them.
-            renderScrollBarButton(renderer,1,x+left,y+top,x+left+16,y+top+height,80,16);
-            renderScrollBarButton(renderer,2,x+left+15,y+top,x+(int)thumbStart,y+top+height,-1,-1);
-            renderScrollBarButton(renderer,3,x-1+(int)thumbStart,y+top,x+1+(int)thumbEnd,y+top+height,16,16);
-            renderScrollBarButton(renderer,4,x+(int)thumbEnd,y+top,x+left+width-15,y+top+height,-1,-1);
-            renderScrollBarButton(renderer,5,x+left+width-16,y+top,x+left+width,y+top+height,96,16);
-		}else{
-			//There are two buttons so draw them.
-			int f=left+width/2;
-            renderScrollBarButton(renderer,1,x+left,y+top,x+1+f,y+top+height,80,16);
-            renderScrollBarButton(renderer,5,x+f,y+top,x+left+width,y+top+height,96,16);
+	if (draw) {
+		if (orientation){
+			//The scrollbar is vertically orientated.
+			if (valuePerPixel > 0){
+				//There are five buttons so draw them.
+				renderScrollBarButton(renderer, 1, x + left, y + top, x + left + width, y + top + 16, 80, 0);
+				renderScrollBarButton(renderer, 2, x + left, y + top + 15, x + left + width, y + (int)thumbStart, -1, -1);
+				renderScrollBarButton(renderer, 3, x + left, y - 1 + (int)thumbStart, x + left + width, y + 1 + (int)thumbEnd, 0, 16);
+				renderScrollBarButton(renderer, 4, x + left, y + (int)thumbEnd, x + left + width, y + top + height - 15, -1, -1);
+				renderScrollBarButton(renderer, 5, x + left, y + top + height - 16, x + left + width, y + top + height, 96, 0);
+			} else{
+				//There are two buttons so draw them.
+				int f = top + height / 2;
+				renderScrollBarButton(renderer, 1, x + left, y + top, x + left + width, y + 1 + f, 80, 0);
+				renderScrollBarButton(renderer, 5, x + left, y + f, x + left + width, y + top + height, 96, 0);
+			}
+		} else{
+			//The scrollbar is horizontally orientated.
+			if (valuePerPixel > 0){
+				//There are five buttons so draw them.
+				renderScrollBarButton(renderer, 1, x + left, y + top, x + left + 16, y + top + height, 80, 16);
+				renderScrollBarButton(renderer, 2, x + left + 15, y + top, x + (int)thumbStart, y + top + height, -1, -1);
+				renderScrollBarButton(renderer, 3, x - 1 + (int)thumbStart, y + top, x + 1 + (int)thumbEnd, y + top + height, 16, 16);
+				renderScrollBarButton(renderer, 4, x + (int)thumbEnd, y + top, x + left + width - 15, y + top + height, -1, -1);
+				renderScrollBarButton(renderer, 5, x + left + width - 16, y + top, x + left + width, y + top + height, 96, 16);
+			} else{
+				//There are two buttons so draw them.
+				int f = left + width / 2;
+				renderScrollBarButton(renderer, 1, x + left, y + top, x + 1 + f, y + top + height, 80, 16);
+				renderScrollBarButton(renderer, 5, x + f, y + top, x + left + width, y + top + height, 96, 16);
+			}
 		}
 	}
 }
