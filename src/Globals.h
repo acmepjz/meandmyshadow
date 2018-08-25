33fc42be8b1260a474ff05d039b258f981ed0f9e
acme_pjz <acme_pjz@hotmail.com>
2018-08-26 00:59:01 +0800

    bump the version to V0.5 beta

--- src/Globals.h
+++ src/Globals.h
@@ -70,7 +70,8 @@ const Uint32 AMASK=0xFF000000;
 /*#endif*/
 
 //String containing the version, used in the titelbar.
-const std::string version="V0.5 Development version";
+//const std::string version="V0.5 Development version";
+const std::string version = "V0.5 Beta";
 
 //The height of the current level.
 extern int LEVEL_HEIGHT;
