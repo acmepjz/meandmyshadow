acmepjz/meandmyshadow

commit 08bd7b763ef92386c5df9bbed6b3824349df339d

reverts some changes in commit 5f03ae5 to fix the block:setLocation() and block:moveTo() bug

src/Game.cpp
@@ -538,6 +538,26 @@ void Game::logic(ImageManager& imageManager, SDL_Renderer& renderer){
 	//Add one tick to the time.
 	time++;
 
+	//NOTE: This code reverts some changes in commit 5f03ae5.
+	//This is part of old prepareFrame() code.
+	//This is needed since otherwise the script function block:setLocation() and block:moveTo() are completely broken.
+	//Later we should rewrite collision system completely which will remove this piece of code.
+	//NOTE: In new collision system the effect of dx/dy/xVel/yVel should only be restricted in one frame.
+	for (auto obj : levelObjects) {
+		switch (obj->type) {
+		default:
+			obj->dx = obj->dy = obj->xVel = obj->yVel = 0;
+			break;
+		case TYPE_PUSHABLE:
+			//NOTE: Currently the dx/dy/etc. of pushable blocks are still carry across frames, in order to make the collision system work correct.
+			break;
+		case TYPE_CONVEYOR_BELT: case TYPE_SHADOW_CONVEYOR_BELT:
+			//NOTE: We let the conveyor belt itself to reset its xVel/yVel.
+			obj->dx = obj->dy = 0;
+			break;
+		}
+	}
+
 	//Process delay execution scripts.
 	getScriptExecutor()->processDelayExecution();
 

src/Block.h
@@ -27,12 +27,14 @@
 #include <vector>
 #include <SDL.h>
 
+class Game; // ad-hoc
 class LevelEditor;
 class AddRemoveGameObjectCommand;
 class AddRemovePathCommand;
 class BlockScriptAPI;
 
 class Block: public GameObject, public ScriptUserClass<'B','L','O','K',Block>{
+	friend class Game; // ad-hoc
 	friend class LevelEditor;
 	friend class AddRemoveGameObjectCommand;
 	friend class AddRemovePathCommand;

commit 3d3c9ae8ad230ec0c66caa5dedeefd812877e215

fixed a big bad bug of pushable block with moving block introduced in commit 08bd7b7 also sorts pushable blocks before doing collision test which serves as an ad-hoc workaround for <https://forum.freegamedev.net/viewtopic.php?f=48&t=8047#p77692>

src/Game.cpp
@@ -557,6 +557,9 @@ void Game::logic(ImageManager& imageManager, SDL_Renderer& renderer){
 		}
 	}
 
+	//NOTE2: The above code breaks pushable block with moving block in most cases,
+	//more precisely, if the pushable block is processed before the moving block then things may be broken.
+
 	//Process delay execution scripts.
 	getScriptExecutor()->processDelayExecution();
 
@@ -605,9 +608,34 @@ void Game::logic(ImageManager& imageManager, SDL_Renderer& renderer){
 		//Send GameObjectEvent_OnEnterFrame event to the script
 		levelObjects[i]->onEvent(GameObjectEvent_OnEnterFrame);
 	}
-	for(unsigned int i=0;i<levelObjects.size();i++){
-		//Let the gameobject handle movement.
-		levelObjects[i]->move();
+	//Let the gameobject handle movement.
+	{
+		std::vector<Block*> pushableBlocks;
+
+		//First we process blocks which are not pushable blocks.
+		for (auto o : levelObjects) {
+			if (o->type == TYPE_PUSHABLE) {
+				pushableBlocks.push_back(o);
+			} else {
+				o->move();
+			}
+		}
+
+		//Sort pushable blocks by their position, which is an ad-hoc workaround for
+		//<https://forum.freegamedev.net/viewtopic.php?f=48&t=8047#p77692>.
+		std::stable_sort(pushableBlocks.begin(), pushableBlocks.end(),
+			[](const Block* obj1, const Block* obj2)->bool
+		{
+			SDL_Rect r1 = const_cast<Block*>(obj1)->getBox(), r2 = const_cast<Block*>(obj2)->getBox();
+			if (r1.y > r2.y) return true;
+			else if (r1.y < r2.y) return false;
+			else return r1.x < r2.x;
+		});
+
+		//Now we process pushable blocks.
+		for (auto o : pushableBlocks) {
+			o->move();
+		}
 	}
 	//Also update the scenery.
 	for (auto it = sceneryLayers.begin(); it != sceneryLayers.end(); ++it){