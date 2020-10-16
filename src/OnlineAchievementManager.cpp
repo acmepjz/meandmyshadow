9eeb4b3c4aceca4130fb3eddc07ca949fcb33305
Dennis Payne <dulsi@identicalsoftware.com>
2020-10-15 21:11:37 -0400

    Add missing ifdefs.

--- src/OnlineAchievementManager.cpp
+++ src/OnlineAchievementManager.cpp
@@ -34,7 +34,9 @@ OnlineAchievementManager::OnlineAchievementManager(){
 }
 
 OnlineAchievementManager::~OnlineAchievementManager(){
+#ifdef GAMERZILLA
 	GamerzillaQuit();
+#endif
 }
 
 void OnlineAchievementManager::registerAchievements(){
